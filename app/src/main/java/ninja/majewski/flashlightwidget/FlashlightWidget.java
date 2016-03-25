818f1470fa6ce783a0e32b640bf8bb25db30e964
Krzysztof Majewski <krzysztof@majewski.ninja>
2016-03-25 18:58:07 +0100

    Adding rebuilded project

---
+++ app/src/main/java/ninja/majewski/flashlightwidget/FlashlightWidget.java
@@ -0,0 +1,40 @@
+package ninja.majewski.flashlightwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class FlashlightWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent receiver = new Intent(context, FlashLightReceiver.class);
+        receiver.setAction("majewski.ninja.flashlightwidget");
+        receiver.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetId);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, receiver, 0);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.flashlight_widget);
+        views.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+    }
+}
